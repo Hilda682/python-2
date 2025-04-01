fecadfe6aa8c3b6165c2bdca3832bcfa1ac75740
Hilda682 <joyh86582@gmail.com>
2025-04-01 16:07:36 +0300

    salary

--- calculator.py
+++ calculator.py
@@ -1,17 +1,13 @@
 name=input("enter your name")
 print("my name is",name)
+children=input("enter the number of children")
+children=int(children)
+print("number of children")
 salary=input("enter amount")
 salary=int(salary)
 print("my salary is",salary )
-
-
-
-
-
-
 if salary <0:
     print("invalid entry")
-
 elif salary > 10000 and salary <= 200000:
     tax =0.3*salary
     print(" your tax rate is :",tax)
@@ -21,6 +17,11 @@ elif salary >20000 and salary <=50000:
 elif salary > 50000:
     tax=0.6*salary
     print(" your tax rate is :",tax)
+net_income=salary-tax
+if children>3:
+    net_income=net_income-1000*children
+else:
+    net_income=net_income    
 
-
+print("net_income is  :",net_income)
 
